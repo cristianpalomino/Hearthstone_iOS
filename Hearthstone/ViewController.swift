fbe7fd791c22dac208616d461de1f34f513302ca
Cristian <cristian.palomino@aerolab.com.ar>
2016-05-01 02:40:20 -0500

    Initial Commit

---
+++ Hearthstone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hearthstone
+//
+//  Created by Cristian on 1/05/16.
+//  Copyright © 2016 Cristian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
